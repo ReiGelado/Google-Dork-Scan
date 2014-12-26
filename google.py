62f27d47b4b9c8476aa106d37f25cadbb398f692
ReiGelado <ReiGelado@users.noreply.github.com>
2014-12-26 21:45:34 -0200

    ###Atualização v0.6

--- google.py
+++ google.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
+#!C:/Python27/
 import argparse
-import urllib2
 import json
 import urllib
-import cookielib
+import conexao #arquivo local
 from time import sleep
 from sys import exit
 from random import choice
@@ -11,82 +11,74 @@ from random import choice
 parser = argparse.ArgumentParser()
 parser.add_argument('--dork',action = 'store', dest = 'dork' , required = True , help = 'Dork que voce vai utilizar na pesquisa do google :)\nExemplo:\ninurl:noticia.php?id=')
 parser.add_argument('--arquivo',action = 'store', dest = 'arquivo', required = True, help = 'Nome do arquivo que voce vai salvar as urls....')
-parser.add_argument('--paginas', dest='pagina', action='store',required = False,help = 'Quantas paginas voce quer de pequisa....')
 parser.add_argument('--reigelado-mode',dest = 'reigelado', action = 'store_true',required = False,help = 'Modo especial do reigelado que voce consegue mais resultados :)\nObs na hora de usar a dork nao coloque numero depois do = :)')
+parser.add_argument('--proxy-tipo',dest = 'tipo_proxy',action = 'store',required = False,help = 'Tipo de proxy...Https/Https/Socks5/4')
+parser.add_argument('--proxy-ip',dest = 'ip_proxy',action = 'store' , required = False,help = 'Ip do Proxy ou Dns :)')
+parser.add_argument('--useragent-proxy',dest = 'user_agent_proxy',action = 'store',required = False,help = 'User-Agent do Proxy :)')
 parser.add_argument('--useragent-escolha',dest = 'user_agent_1',action = 'store',required = False,help = 'Voce define o User-Agent que o script vai usar :)')
 parser.add_argument('--useragent-random',dest = 'user_agent_2',action = 'store_true',required = False,help = 'O programa escolhe o user agente de forma randomica :D')
 parser.add_argument('--useragent-txt',dest = 'user_agent_3',action = 'store',required = False,help = 'Voce define um .txt com o UserAgent  o script identifica por linha!')
 parser.set_defaults(reigelado=False)
+parser.set_defaults(tipo_proxy = "")
+parser.set_defaults(ip_proxy = "")
+parser.set_defaults(user_agent_proxy="")
 parser.set_defaults(user_agent_1="")
 parser.set_defaults(user_agent_2=False)
 parser.set_defaults(user_agent_3="")
-parser.set_defaults(pagina="")
 arguments = parser.parse_args()
 
-lista_keys = ['AIzaSyCwWrjytJ-qmamxkeEFdAiUT9pTUClj0VU','AIzaSyAh8i756jTGxQ56RJb5RL9HTNq9UJfBlWc','AIzaSyAcjg1Nq2EhhgYMUOSN2NqCwixbsj2bKXo','AIzaSyBUfvpfclLWB-0LXoSmcrFw1ieX3rexyFw','AIzaSyAPq1_9olu0cNmTSMSQEpcNWahK8Q6Edvs','AIzaSyAMYYUdMTNDabQS8kOHibOKWoT29hMp6Dg','AIzaSyBSn-Ylr2CfbEBMxgLlDu803p6PzdTjj04','AIzaSyCjcZ4cNcof8_eUK213RXS0MpHhl3akHkE','AIzaSyAMYYUdMTNDabQS8kOHibOKWoT29hMp6Dg','AIzaSyA-4q8QaPuu6d8iXJYOH91CMuNaVrVJ2-M']
-lista_user = ['Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/1.0.154.53 Safari/525.19','Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.10 (KHTML, like Gecko) Ubuntu/10.10 Chromium/8.0.552.237 Chrome/8.0.552.237 Safari/534.10','Opera/5.11 (Windows 98; U) [en]','Mozilla/5.0 (Macintosh; U; PPC Mac OS X; fi-fi) AppleWebKit/420+ (KHTML, like Gecko) Safari/419.3','Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.1b2pre) Gecko/20081015 Fennec/1.0a1','Mozilla/5.0 (Ubuntu; Mobile) WebKit/537.21','Mozilla/4.0 (compatible; MSIE 5.0; Windows NT;)','Mozilla/5.0 (Windows NT 6.1; rv:20.0) Gecko/20100101 Firefox/20.0 IceDragon/20.0.1.14']
-
-def funcao_acessa_api(string):
+def funcao_acessa_api(url):
+	#Um breve ensinamento gafanhotos,se o script bugar e o seu nome nao estiver na classe pode festejar que a culpa nao e sua U.U
+	ua = conexao.ReiGelado()
 	try:
-		api = urllib2.Request(string)
-		cj = cookielib.MozillaCookieJar()
-		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-		if arguments.user_agent_2 == True:
-			print '[+]Escolhendo User-Agent de forma randomica....\n'
-			user_random = choice(lista_user)
-			print '[+]O user agent e %szn' % user_random
-			api.add_header('User-agent',user_random)
-			return opener.open(api)
-		else:
-			pass
 		if arguments.user_agent_1 == "":
 			pass
 		else:
-			api.add_header('User-agent',arguments.user_agent_1)
-			print '[+]User-Agent: %s \n'  % arguments.user_agent_1
-			return opener.open(api)
-		if arguments.user_agent_3 == "":
-			pass
-		else: 
-			reigelado_gostoso = []
-			print '\n[+]Lendo seu .txt...\n'
-			for reigelado in open(arguments.user_agent_3,'r'):
-				reigelado.strip()
-				reigelado_gostoso.append(reigelado)
-			print '+]Escolhendo user-agent....\n'
-			user_agent_4 = choice(reigelado_gostoso) 
-			print '[+]User-agent escolhido %s\n' % user_agent_4
-			api.add_header('User-agent',user_agent_4)
-			return opener.open(api)
-		if arguments.user_agent_1 == "" or arguments.user_agent_2 == False or arguments.user_agent_3 == "":
-			print '[+]Conectando.....\n'
-			print '[+]Usando user-agent padrao do script....\n'
-			print '[+]User-Agent:Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/1.0.154.53 Safari/525.19\n'
-			api.add_header('User-agent','Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/1.0.154.53 Safari/525.19')
-			return opener.open(api)
+			return ua.user_agent_1(url,arguments.user_agent_3)
+		if arguments.user_agent_2 == True:
+			return ua.user_agent_2(url)
 		else:
 			pass
-	except:
+		if arguments.user_agent_3 == "":
+			pass
+		else:
+			return ua.user_agent_3(url,arguments.user_agent_3)
+		if arguments.ip_proxy == "" or arguments.tipo_proxy == "":
+			pass
+		else:
+			print '[+]Iniciando Conexao...\n'
+			print '[+]Proxy-Ip:%s\n' % arguments.ip_proxy
+			print '[+]Tipo de Proxy:%s\n' % arguments.tipo_proxy
+			if arguments.user_agent_proxy == "":
+				return ua.user_proxy(url,arguments.ip_proxy,arguments.tipo_proxy)
+			else:
+				return ua.proxy_user(url,arguments.ip_proxy,arguments.tipo_proxy,arguments.user_agent_proxy)
+		if arguments.user_agent_1 == "" or arguments.user_agent_2 == False or arguments.user_agent_3 == "":
+			return ua.user_off(url)
+		else:
+			pass
+	except KeyError:
 		print '\n[+]ERRO 403 TENTE NOVAMENTE......\n'
 		print '[+]Bye ;)'
 		exit()
 
 print '''
-......::::::::Gooogle Dork Scan V0.5 build 1 by Rei_Gelado::::::::......
-......::::::::Forum:http://caveiratech.com/forum/::::::::......
-......::::::::ATENCAO : NA HORA DE USAR A DORK COLOQUE O INURL::::::::::..... 
-           OBS:NO REIGELADO-MODE USE DORKS COM NUMEROS NO FINAL 
-                              EXEMPLO:
+                    ......::::::::Google Dork Scan V0.6 by Rei_Gelado::::::::......
+                    ......::::::::Forum:http://caveiratech.com/forum/::::::::......
+                                            EX:
     script.py --dork=inurl:videos_pornos.php?id=1 --arquivo=caveiratech.txt
-    script.py --dork=inurl:videos_pornos.php?id=1 --arquivo=caveiratech.txt --paginas=2 #beta
     script.py --dork=inurl:videos_pornos.php?id=  --arquivo=caveiratech.txt --reigelado-mode 
     script.py --dork=inurl:videos_pornos.php?id=  --arquivo=caveiratech.txt --useragent-random
     script.py --dork=inurl:videos_pornos.php?id=  --arquivo=caveiratech.txt --useragent-escolha="Seu User-Agent Aqui" #Use Aspas
     script.py --dork=inurl:videos_pornos.php?id=  --arquivo=caveiratech.txt --useragent-txt=meu_arquivo.txt #beta
+    script.py --dork=inurl:videos_pornos.php?id=  --arquivo=caveiratech.txt --proxy-ip="127.0.0.1:2424" --proxy-tipo="https" #https,socks5,socks4
+    script.py --dork=inurl:videos_pornos.php?id=  --arquivo=caveiratech.txt --proxy-ip="127.0.0.1:2424" --proxy-tipo="https" --useragent-proxy="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US)"
 
 '''
-print '[+]Conectando....... :)\n'
+lista_keys = []
 
+for keys2 in open('keys.txt','r'):
+	lista_keys.append(keys2.strip())
 
 key_key = choice(lista_keys) 
 
@@ -137,7 +129,7 @@ if arguments.reigelado == True:
 			print '[+] + 1 dork adicionada....\n'
 else:
 	pass
-if arguments.pagina == "":
+if arguments.reigelado == False:
 	print '[+]Salvando dados.....\n'
 	for x in range(0,10):
 		escrever = open(arguments.arquivo,'a')
@@ -152,18 +144,4 @@ if arguments.pagina == "":
 	print '[+]Scan finalizado Bye :)\n'
 	exit()
 else:
-	while True:
-		n = n + 1
-		valor = limpo2 + '&start=' + str(n) + '&q=' + arguments.dork
-		html = funcao_acessa_api(valor)
-		json_api2 = json.loads(html)
-		for x in range(0,10):
-			escrever = open(arguments.arquivo,'a')
-			escrever.write(json_api2['items'][x]['link'] + '\n')
-			escrever.close()
-			print '[+]+ 1 dork adicionada...\n'
-		if n == arguments.pagina:
-			print '[+]Scan Finalizado Bye :)'
-			exit()
-		else:
-			pass
+	pass
